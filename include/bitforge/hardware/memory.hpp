bb39a328bde3d72636d2ab66aebc78faa7008d55
harsh <hk5324519@gmail.com/>
2026-09-09 00:16:40 +0530

    DFF done testing is remaning

--- include/bitforge/hardware/memory.hpp
+++ include/bitforge/hardware/memory.hpp
@@ -1,29 +1,44 @@
-#ifndef MEMORY_HPP
-#define MEMORY_HPP
-class SR_LATCH
-{
-private:
-  bool Q;
-  bool Qn;
+#pragma once
 
-public:
-  SR_LATCH();
-  void evaluate(bool S, bool R);
-  bool getQ();
-  bool getQn();
-};
-class DFF
+namespace bitforge::hardware
 {
-private:
-  SR_LATCH master;
-  SR_LATCH slave;
-  bool Q;
-  bool Qn;
+  class SR_LATCH
+  {
+  private:
+    bool Q;
+    bool Qn;
 
-public:
-  DFF();
-  void evaluate(bool D, bool clock);
-  bool getQ();
-  bool getQn();
-};
-#endif
+  public:
+    SR_LATCH();
+    void evaluate(bool S, bool R);
+    bool getQ();
+    bool getQn();
+  };
+  class JKFF
+  {
+  private:
+    SR_LATCH master;
+    SR_LATCH slave;
+    bool Q;
+    bool Qn;
+
+  public:
+    JKFF();
+    void evaluate(bool J, bool K, bool clock);
+    bool getQ();
+    bool getQn();
+  };
+  class DFF
+  {
+  private:
+    bool Q;
+    bool Qn;
+    JKFF jk;
+
+  public:
+    DFF();
+    void evaluate(bool D, bool clock);
+    bool getQ();
+    bool getQn();
+  };
+}
--- src/hardware/memory.cpp
+++ src/hardware/memory.cpp
@@ -10,24 +10,21 @@ namespace bitforge::hardware
 
   void SR_LATCH::evaluate(bool S, bool R)
   {
-    Q = Nand(S, Qn);
-    Qn = Nand(R, Q);
+
+    bool nextQ = Nand(S, Qn);
+    bool nextQn = Nand(R, Q);
+    Q = nextQ;
+    Qn = nextQn;
   }
 
   bool SR_LATCH::getQ() { return Q; }
   bool SR_LATCH::getQn() { return Qn; }
 
-  DFF::DFF()
+  JKFF::JKFF() {}
+  void JKFF::evaluate(bool J, bool K, bool clock)
   {
-
-    Q = true;
-
-    Qn = false;
-  }
-  void DFF::evaluate(bool D, bool clock)
-  {
-    bool J = D;
-    bool K = Not(D);
+    Q = master.getQ();
+    Qn = master.getQn();
     master.evaluate(And(Qn, And(J, clock)), And(Q, And(K, clock)));
     bool Q_m = master.getQ();
     bool Qn_m = master.getQn();
@@ -35,4 +32,17 @@ namespace bitforge::hardware
     Q = slave.getQ();
     Qn = slave.getQn();
   }
+  bool JKFF::getQ() { return Q; }
+  bool JKFF::getQn() { return Qn; }
+  DFF::DFF() {}
+  void DFF::evaluate(bool D, bool clock)
+  {
+    bool J = D;
+    bool K = Not(D);
+    jk.evaluate(J, K, clock);
+    Q = jk.getQ();
+    Qn = jk.getQn();
+  }
+  bool DFF::getQ() { return Q; }
+  bool DFF::getQn() { return Qn; }
 }
