85ae8ef01c4dbba4176e99c796d436d21d12ad93
harsh <hk5324519@gmail.com/>
2026-09-08 23:46:17 +0530

    JKFF making

--- src/hardware/memory.cpp
+++ src/hardware/memory.cpp
@@ -1,31 +1,38 @@
 #include "bitforge/hardware/memory.hpp"
 #include "bitforge/hardware/gates.hpp"
-
-SR_LATCH::SR_LATCH()
+namespace bitforge::hardware
 {
-  Q = false;
-  Qn = true;
-}
+  SR_LATCH::SR_LATCH()
+  {
+    Q = true;
+    Qn = false;
+  }
 
-void SR_LATCH::evaluate(bool S, bool R)
-{
-  Q = bitforge::hardware::Nand(S, Qn);
-  Qn = bitforge::hardware::Nand(R, Q);
-}
+  void SR_LATCH::evaluate(bool S, bool R)
+  {
+    Q = Nand(S, Qn);
+    Qn = Nand(R, Q);
+  }
 
-bool SR_LATCH::getQ() { return Q; }
-bool SR_LATCH::getQn() { return Qn; }
+  bool SR_LATCH::getQ() { return Q; }
+  bool SR_LATCH::getQn() { return Qn; }
 
-DFF::DFF()
-{
+  DFF::DFF()
+  {
 
-  Q = false;
+    Q = true;
 
-  Qn = false;
-}
-void DFF::evaluate(bool D, bool clock)
-{
-  bool R = bitforge::hardware::Not(D);
-
-  bool S = D;
+    Qn = false;
+  }
+  void DFF::evaluate(bool D, bool clock)
+  {
+    bool J = D;
+    bool K = Not(D);
+    master.evaluate(And(Qn, And(J, clock)), And(Q, And(K, clock)));
+    bool Q_m = master.getQ();
+    bool Qn_m = master.getQn();
+    slave.evaluate(And(Q_m, Not(clock)), And(Qn_m, Not(clock)));
+    Q = slave.getQ();
+    Qn = slave.getQn();
+  }
 }
