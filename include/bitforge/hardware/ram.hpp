21fe13b8281a0fa771ee641f89226a3e5549fad4
harsh <hk5324519@gmail.com/>
2026-09-13 23:47:38 +0530

    Adding Ram8 test cases

--- include/bitforge/hardware/ram.hpp
+++ include/bitforge/hardware/ram.hpp
@@ -1,6 +1,13 @@
 #pragma once
+#include "bitforge/hardware/registers.hpp"
 #include "bitforge/hardware/types.hpp"
-
 namespace bitforge::hardware {
-Bits16 Ram8(Bits16 &inp, Bits3 &address, bool load);
-}
+class RAM {
+private:
+  std::array<Register, 8> registers8;
+
+public:
+  RAM();
+  Bits16 Ram8(Bits16 &inp, const Bits3 &address, bool load);
+};
+} // namespace bitforge::hardware
--- src/hardware/ram.cpp
+++ src/hardware/ram.cpp
@@ -1,16 +1,16 @@
 #include "bitforge/hardware/ram.hpp"
 #include "bitforge/hardware/mux.hpp"
-#include "bitforge/hardware/registers.hpp"
+#include "bitforge/hardware/types.hpp"
 namespace bitforge::hardware {
-Bits16 Ram8(Bits16 &inp, Bits3 &address, bool load) {
-
-  Register register1;
-  Register register2;
-  Register register3;
-  Register register4;
-  Register register5;
-  Register register6;
-  Register register7;
-  Register register8;
+RAM::RAM() {}
+Bits16 RAM::Ram8(Bits16 &inp, const Bits3 &address, bool load) {
+  Bits8 write_address = Dmux8Way(load, address);
+  for (int pos = 0; pos < 8; pos++) {
+    registers8[pos].evaluate(inp, write_address[pos]);
+  }
+  return Mux8Way16(registers8[0].getQ(), registers8[1].getQ(),
+                   registers8[2].getQ(), registers8[3].getQ(),
+                   registers8[4].getQ(), registers8[5].getQ(),
+                   registers8[6].getQ(), registers8[7].getQ(), address);
 }
 } // namespace bitforge::hardware
