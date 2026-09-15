ce7a9c3e2d5476250b8a708993b053a0ef1bec95
harsh <hk5324519@gmail.com/>
2026-09-15 23:20:10 +0530

    Adding RAM512, RAM4K, RAM16K with test cases

--- include/bitforge/hardware/types.hpp
+++ include/bitforge/hardware/types.hpp
@@ -7,6 +7,9 @@ using Bits3 = std::array<bool, 3>;
 using Bits4 = std::array<bool, 4>;
 using Bits6 = std::array<bool, 6>;
 using Bits8 = std::array<bool, 8>;
+using Bits9 = std::array<bool, 9>;
+using Bits12 = std::array<bool, 12>;
+using Bits14 = std::array<bool, 14>;
 using Bits16 = std::array<bool, 16>;
 
 } // namespace bitforge::hardware
--- src/hardware/ram.cpp
+++ src/hardware/ram.cpp
@@ -16,12 +16,16 @@ Bits16 RAM8::evaluate(const Bits16 &inp, const Bits3 &address, bool load) {
 
 RAM64::RAM64() {}
 Bits16 RAM64::evaluate(const Bits16 &inp, const Bits6 &address, bool load) {
-  const Bits3 ram_address{
+  const Bits3 ram_selection{
       address[0],
       address[1],
       address[2],
   };
-  const Bits3 ram_selection{address[3], address[4], address[5]};
+  const Bits3 ram_address{
+      address[3],
+      address[4],
+      address[5],
+  };
   Bits8 selected_ram = Dmux8Way(load, ram_selection);
   return Mux8Way16(rams[0].evaluate(inp, ram_address, selected_ram[0]),
                    rams[1].evaluate(inp, ram_address, selected_ram[1]),
@@ -31,6 +35,69 @@ Bits16 RAM64::evaluate(const Bits16 &inp, const Bits6 &address, bool load) {
                    rams[5].evaluate(inp, ram_address, selected_ram[5]),
                    rams[6].evaluate(inp, ram_address, selected_ram[6]),
                    rams[7].evaluate(inp, ram_address, selected_ram[7]),
+                   ram_selection);
+}
+
+RAM512::RAM512() {}
+Bits16 RAM512::evaluate(const Bits16 &inp, const Bits9 &address, bool load) {
+  const Bits6 ram_address{
+      address[3], address[4], address[5], address[6], address[7], address[8],
+  };
+  const Bits3 ram_selection{
+      address[0],
+      address[1],
+      address[2],
+  };
+  Bits8 selected_ram = Dmux8Way(load, ram_selection);
+  return Mux8Way16(rams[0].evaluate(inp, ram_address, selected_ram[0]),
+                   rams[1].evaluate(inp, ram_address, selected_ram[1]),
+                   rams[2].evaluate(inp, ram_address, selected_ram[2]),
+                   rams[3].evaluate(inp, ram_address, selected_ram[3]),
+                   rams[4].evaluate(inp, ram_address, selected_ram[4]),
+                   rams[5].evaluate(inp, ram_address, selected_ram[5]),
+                   rams[6].evaluate(inp, ram_address, selected_ram[6]),
+                   rams[7].evaluate(inp, ram_address, selected_ram[7]),
+                   ram_selection);
+}
+
+RAM4K::RAM4K() {}
+Bits16 RAM4K::evaluate(const Bits16 &inp, const Bits12 &address, bool load) {
+  const Bits9 ram_address{address[3], address[4],  address[5],
+                          address[6], address[7],  address[8],
+                          address[9], address[10], address[11]};
+  const Bits3 ram_selection{
+      address[0],
+      address[1],
+      address[2],
+  };
+  Bits8 selected_ram = Dmux8Way(load, ram_selection);
+  return Mux8Way16(rams[0].evaluate(inp, ram_address, selected_ram[0]),
+                   rams[1].evaluate(inp, ram_address, selected_ram[1]),
+                   rams[2].evaluate(inp, ram_address, selected_ram[2]),
+                   rams[3].evaluate(inp, ram_address, selected_ram[3]),
+                   rams[4].evaluate(inp, ram_address, selected_ram[4]),
+                   rams[5].evaluate(inp, ram_address, selected_ram[5]),
+                   rams[6].evaluate(inp, ram_address, selected_ram[6]),
+                   rams[7].evaluate(inp, ram_address, selected_ram[7]),
+                   ram_selection);
+}
+
+RAM16K::RAM16K() {}
+Bits16 RAM16K::evaluate(const Bits16 &inp, const Bits14 &address, bool load) {
+  const Bits12 ram_address{address[2],  address[3],  address[4],
+                           address[5],  address[6],  address[7],
+                           address[8],  address[9],  address[10],
+                           address[11], address[12], address[13]};
+  const Bits2 ram_selection{
+      address[0],
+      address[1],
+  };
+
+  Bits4 selected_ram = Dmux4Way(load, ram_selection);
+  return Mux4Way16(rams[0].evaluate(inp, ram_address, selected_ram[0]),
+                   rams[1].evaluate(inp, ram_address, selected_ram[1]),
+                   rams[2].evaluate(inp, ram_address, selected_ram[2]),
+                   rams[3].evaluate(inp, ram_address, selected_ram[3]),
                    ram_selection);
 }
 
